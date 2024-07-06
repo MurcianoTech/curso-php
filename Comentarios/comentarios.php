96851d3ed56bdea636142301f2fdba90c982f9a7
Alejandro Valenzuela <alejandro.cieza@gmail.com>
2024-07-06 13:25:44 +0200

    [ADD] Added comentarios php file

---
+++ Comentarios/comentarios.php
@@ -0,0 +1,15 @@
+<?php
+
+// Mi primer comentario comentario
+echo 'Mi primer comentario';
+
+# Mi segundo comentario
+echo 'Mi segundo comentario echo';
+
+/*
+Author: Alejandro
+Date: 6/7/2024
+*/
+echo 'Comentario varios líneas';
+
+?>
