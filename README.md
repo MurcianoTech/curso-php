486a0c79caa855eb0bca07b8eae6f99d482dd0f6
Alejandro Valenzuela <alejandro.cieza@gmail.com>
2024-07-07 13:37:41 +0200

    [ADD] Added Variables lesson and Updating Readme file

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Clase 1 -> Añadido Comentarios.php 
 ## Clase 2 -> Añadido TiposDeDatos.php
+## Clase 3 -> Añadido Variables.php
---
+++ Variables/variables.php
@@ -0,0 +1,25 @@
+<?php
+
+$numero = 1;
+echo $numero;
+var_dump($numero);
+
+$cadena = 'Mi primer valor de cadena en variable';
+echo $cadena;
+var_dump($cadena);
+
+// Nomenclatura UpperCase
+$CADENA = 'CADENA';
+var_dump($CADENA);
+
+// Nomenclatura CamelCase
+$cadenaCaracteres = 'Cadena Caracteres';
+
+var_dump($cadenaCaracteres);
+
+// Nomenclatura SnakeCase
+$cadena_caracteres = 'Cadena de Caracteres';
+
+var_dump($cadena_caracteres);
+
+?>
